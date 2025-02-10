c1c772d3daccc02a43f39b7a19b45c0722163493
Sushanth N <sushanthnandeti96@gmail.com>
2025-02-09 21:48:48 -0500

    WaitListEntryDetail api route added

--- src/waitlists/api.py
+++ src/waitlists/api.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from ninja import Router 
-from .schemas import WaitListEntryListSchema
+from .schemas import WaitListEntryListSchema, WaitListEntryDetailSchema
 from typing import List
 from .models import WaitListEntry
 
@@ -9,3 +10,9 @@ router = Router()
 def list_waitlist_entries(request):
     qs = WaitListEntry.objects.all()
     return qs
+
+
+@router.get("{entry_id}", response=WaitListEntryDetailSchema)
+def get_waitlist_entry(request, entry_id : int):
+    obj = get_object_or_404(WaitListEntry, id = entry_id)
+    return obj
