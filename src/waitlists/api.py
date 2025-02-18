51c3e9d8013deb5b90f338a9108ff59dae5f8cf5
Sushanth N <sushanthnandeti96@gmail.com>
2025-02-17 22:15:28 -0500

    Security factor added for wailist api route

--- src/waitlists/api.py
+++ src/waitlists/api.py
@@ -1,18 +1,19 @@
 from django.shortcuts import get_object_or_404
 from ninja import Router 
+from ninja_jwt.authentication import JWTAuth
 from .schemas import WaitListEntryListSchema, WaitListEntryDetailSchema
 from typing import List
 from .models import WaitListEntry
 
 router = Router()
 
-@router.get("", response=List[WaitListEntryListSchema])
+@router.get("", response=List[WaitListEntryListSchema],auth=JWTAuth())
 def list_waitlist_entries(request):
     qs = WaitListEntry.objects.all()
     return qs
 
 
-@router.get("{entry_id}", response=WaitListEntryDetailSchema)
+@router.get("{entry_id}", response=WaitListEntryDetailSchema,auth=JWTAuth())
 def get_waitlist_entry(request, entry_id : int):
     obj = get_object_or_404(WaitListEntry, id = entry_id)
     return obj
