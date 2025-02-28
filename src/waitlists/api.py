5bb9a2735d5a236242c267b217ca0f220ba8cafd
Sushanth N <sushanthnandeti96@gmail.com>
2025-02-27 21:59:40 -0500

    helpers module added for authentication

--- src/waitlists/api.py
+++ src/waitlists/api.py
@@ -1,19 +1,33 @@
 from django.shortcuts import get_object_or_404
 from ninja import Router 
 from ninja_jwt.authentication import JWTAuth
-from .schemas import WaitListEntryListSchema, WaitListEntryDetailSchema
+from .schemas import WaitListEntryListSchema, WaitListEntryDetailSchema,WaitListEntryCreateSchema
 from typing import List
 from .models import WaitListEntry
 
+import helpers
+
 router = Router()
 
-@router.get("", response=List[WaitListEntryListSchema],auth=JWTAuth())
+def allow_annon(request):
+    if not request.user.is_authenticated:
+        return True
+
+@router.get("", response=List[WaitListEntryListSchema],auth=helpers.api_auth_user_required)
 def list_waitlist_entries(request):
     qs = WaitListEntry.objects.all()
     return qs
 
+# api/waitlist/
 
-@router.get("{entry_id}", response=WaitListEntryDetailSchema,auth=JWTAuth())
+@router.post("", response=WaitListEntryDetailSchema, auth=[helpers.helpers.api_auth_user_or_annon])
+def create_waitlist_entry(request, data: WaitListEntryCreateSchema):
+    obj = WaitListEntry(**data.dict())
+    print(request.user)
+    obj.save()
+    return obj
+
+@router.get("{entry_id}", response=WaitListEntryDetailSchema,auth=helpers.api_auth_user_required)
 def get_waitlist_entry(request, entry_id : int):
     obj = get_object_or_404(WaitListEntry, id = entry_id)
     return obj
